0420f9a8e2fc41508b3e7c589899ac69469e0c77
sanket.maru <sanket.maru@weboniselab.com>
2015-06-01 18:03:54 +0530

    added test and logic for rotating , test failing

---
+++ index.js
@@ -0,0 +1,39 @@
+
+exports.createArray = function(){
+	var collection = new MyArray(arguments);
+	return collection;
+};
+
+exports.rotateByLast = function(array, positions){
+
+	var arrayLength = array.length - 1;
+	var midElement = array.length - positions;
+
+	var tempArr = [];
+
+	tempArr = array.reverse(midElement, arrayLength);
+	tempArr = array.reverse(0, midElement);
+	tempArr = tempArr.reverse(0, tempArr.length);
+
+	return tempArr;
+};
+
+
+function MyArray(){
+
+	var customArray = Object.create(Array.prototype);
+
+	customArray = Array.apply(this, arguments[0]);
+
+	customArray.reverse = function(startIndex, endIndex){
+		var arrayLengthToReverse = startIndex;
+		var tempArr = [];
+		for(var i=endIndex;i >= startIndex;i--){
+			tempArr[arrayLengthToReverse] = this[i];
+			arrayLengthToReverse++;
+		}
+		return tempArr;
+	};
+
+	return customArray;
+}
--- test.js
+++ test.js
@@ -1,18 +1,49 @@
 var expect = require("chai").expect;
+var rotateArray = require("./");
 
 
-describe("test arary", function(){
+describe("test array length", function(){
 
-	describe("test array length", function(){
+	it("should have length more than 1", function(){
+		var arr = [1,2,3];
+		expect(arr.length).to.be.at.least(1);
+	});
 
-		it("should have length more than 1", function(){
 
-			var arr = [1,2,3];
-			expect(arr.length).to.equal(3);
+	/*it("should reverse array from n position", function(){
 
-		});
+		var arr = [1,2,3,4,5,6];
+		var position = 2;
 
-	})
+		var arrayLength = arr.length-1; //5
+		var arrayLengthToReverse = arr.length-position;//4
+		var startArrayLength = arrayLengthToReverse;
 
+		var tempArr = [];
+		for(var i=arrayLength;i >= startArrayLength;i--){
+			tempArr[arrayLengthToReverse] = arr[i];
+			arrayLengthToReverse++;
+		}
+
+		expect(tempArr[5]).to.equal(5);
+
+	});*/
+
+
+	it("should rotate array from 2 position", function(){
+
+		var arr = rotateArray.createArray(1,2,3,4,5,6);
+		var position = 2;
+		var newArr = rotateArray.rotateByLast(arr, position);
+
+		console.log("new array is" + newArr);
+
+		expect(newArr[0]).to.equal(5);
+		expect(newArr[1]).to.equal(6);
+
+	});
 
 });
+
+
+
