40ab3eef14d23e6bf4c69c6679ed32dc9d4019aa
sanket.maru <sanket.maru@weboniselab.com>
2015-06-01 15:39:53 +0530

    added sample test

---
+++ test.js
@@ -0,0 +1,18 @@
+var expect = require("chai").expect;
+
+
+describe("test arary", function(){
+
+	describe("test array length", function(){
+
+		it("should have length more than 1", function(){
+
+			var arr = [1,2,3];
+			expect(arr.length).to.equal(3);
+
+		});
+
+	})
+
+
+});
